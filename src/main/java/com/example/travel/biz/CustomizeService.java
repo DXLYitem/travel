f61e9326f0ab5bc1b256a7579796bdc41c70ec84
244553794 <244553794@qq.com>
2018-05-24 10:14:10 +0800

    2018/5/23 增加查看会员信息

--- src/main/java/com/example/travel/biz/CustomizeService.java
+++ src/main/java/com/example/travel/biz/CustomizeService.java
@@ -2,8 +2,12 @@ package com.example.travel.biz;
 
 import com.example.travel.entity.Customize;
 
+import java.util.List;
+
 public interface CustomizeService {
     public int addCustomize(Customize customize);
 
     public int queryId();
+
+    public List<Customize> listCustomize(String phone);
 }
