aed45b0f56e7d0e525185522bef6926fa7667391
244553794 <244553794@qq.com>
2018-05-24 09:31:29 +0800

    2018/5/23 增加查看会员信息

--- src/main/java/com/example/travel/dao/CustomizeDao.java
+++ src/main/java/com/example/travel/dao/CustomizeDao.java
@@ -1,9 +1,14 @@
 package com.example.travel.dao;
 
 import com.example.travel.entity.Customize;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface CustomizeDao {
     public int submit(Customize customize);
 
     public int selId();
+
+    public List<Customize> selCustomize(@Param("phone") String phone);
 }
